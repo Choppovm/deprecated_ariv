5ddd35a512d374edbf5b77a264a229540133c19e
Choppovm <118126633+Choppovm@users.noreply.github.com>
2025-08-23 19:46:15 +0100

    Update ingame_chatlogs_webhook.js

--- netlify/functions/ingame_chatlogs_webhook.js
+++ netlify/functions/ingame_chatlogs_webhook.js
@@ -1,7 +1,6 @@
 // FILE USES SCP: ROLEPLAY IN-GAME ADDON. SUBJECT TO CHANGE.
 exports.handler = async function(event, context) {
-  const discordWebhook = process.env.DISCORD_WEBHOOK_TEMPSCPRPCHOPLOGS; // temp webhook url
-
+  const discordWebhook = process.env.DISCORD_WEBHOOK_INGAMECHATLOGS;
   if (!discordWebhook) {
     return {
       statusCode: 500,
